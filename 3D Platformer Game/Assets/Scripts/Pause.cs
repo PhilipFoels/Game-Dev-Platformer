99b5c7844c86a79fdb4a9098819183e53bff0d82
Philip Foels <60944590+PhilipFoels@users.noreply.github.com>
2023-04-12 09:54:50 -0700

    button

--- 3D Platformer Game/Assets/Scripts/Pause.cs
+++ 3D Platformer Game/Assets/Scripts/Pause.cs
@@ -6,12 +6,18 @@ public class Pause : MonoBehaviour
 {   
     public GameObject pauseMenu;
     public GameObject ResumeButton;
+    public GameObject ExitButton; 
     // Start is called before the first frame update
     void Start()
     {
         pauseMenu.SetActive(false);
         Button btn = ResumeButton.GetComponent<Button>();
 		btn.onClick.AddListener(TaskOnClick);
+        Button extbtn = ExitButton.GetComponent<Button>();
+        extbtn.onClick.AddListener(ExitOnClick);
+    }
+    void ExitOnClick(){
+        Application.Quit();
     }
     void TaskOnClick(){
         pauseMenu.SetActive(false);
